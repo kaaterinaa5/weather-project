19a8b05050526790f3aa48902cd0f5ae5df2c203
Katia <kaaterinaa.b@gmail.com>
2020-07-23 22:53:23 +0200

    weather project

---
+++ index.js
@@ -0,0 +1,72 @@
+let now = new Date();
+let currentDate = document.querySelector("#current-date");
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+
+function showWeather(response) {
+  console.log(response.data.name);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function searchCity(city) {
+  let apiKey = "cc0c04c662ada3cd15e0c73cbe9dece0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+
+  let searchInput = document.querySelector("#search-text-input");
+  console.log(searchInput.value);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${searchInput.value}`;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+function searchLocation(position) {
+  let apiKey = "cc0c04c662ada3cd15e0c73cbe9dece0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
